f8a610ce1b5362f878fcedbe70126c84b0625165
deepanshu02 <deepanshusethi507@gmail.com>
2024-12-28 16:32:35 +0530

    Added backend code for city_selector

---
+++ Helpify-backend/routes/city.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cityModel from "../models/citydata.js";
+
+const router = express.Router();
+
+router.get('/' , async(req,res)=>{
+    try {
+        const cities = await cityModel.find();
+        console.log("cities",cities);
+        res.json(cities);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+export default router;
