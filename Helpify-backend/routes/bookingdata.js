093e07c334b2dbad0e2c8a97619aef258cb5b79a
deepanshu02 <deepanshusethi507@gmail.com>
2024-12-28 19:36:13 +0530

    Added backend code for booking_data

---
+++ Helpify-backend/routes/bookingdata.js
@@ -0,0 +1,37 @@
+import express from "express";
+import BookingDataModel from '../models/bookingdata.js';
+import { verifyToken } from "../utils/helpers.js";
+
+const router = express.Router();
+
+router.post('/',verifyToken , (req,res)=>{
+    BookingDataModel.addData(
+        req,
+        req.body,
+        (dbRes)=>{
+            res.status(201);           
+            res.send(dbRes);
+        },
+        (dbErr)=>{
+            res.status(500);
+            res.send(dbErr);
+        }
+    )
+})
+
+router.get('/', verifyToken , (req,res)=>{
+    BookingDataModel.getData(
+    req,
+    (dbRes)=>{
+        res.status(201);
+        res.send(dbRes);
+    },
+    (dbErr)=>{
+        res.status(500);
+        res.send(dbErr);
+    }
+
+)
+})
+
+export default router;
