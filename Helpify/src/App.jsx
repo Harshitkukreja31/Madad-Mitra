90ce3655e2e393cb5c646b04bf4f0c2a1536719d
Harshitkukreja31 <harshitkukreja07@gmail.com>
2024-12-30 15:54:35 +0530

    final routing

--- Helpify/src/App.jsx
+++ Helpify/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Header/Navbar.jsx";
+import Home from "./Pages/Home/Home";
+import Services from "./Pages/Services/Services.jsx";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Profile from "./Pages/Profile/Profile.jsx";
+import WorkerRegistration from "./Components/HiringForm/WorkerRegistration.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import AboutUsPage from "./Pages/AboutUs/AboutUsPage.jsx";
+import ContactUsPage from "./Pages/ContactUs/ContactUsPage.jsx";
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+   <>
+  
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/services/:serviceType" element={<Services />} />
+        <Route path="/login"/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/gethired" element={<WorkerRegistration/>}/>
+        <Route path="/aboutus" element={<AboutUsPage/>}/>
+        <Route path="/contactus" element={<ContactUsPage/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+    
+    </>
+  );
+};
 
-export default App
+export default App;
